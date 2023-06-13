f45347247389ec339546058bcfe9fc406cb68733
Victor <victor.marcoci@gmail.com>
2023-06-13 10:22:08 +0300

    fix footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,14 +5,14 @@ const Footer = () => {
   return (
     <div className={s.root}>
       <div className={s.content}>
-        <div className={s.git}>Github</div>
+        <div className={s.git}>
+          Github
+          <a href="https://github.com/marcoci23/news-collector">
+            news-collector
+          </a>
+        </div>
         <div className={s.logo}>
           <span>NEWS COllECTOR</span>
-        </div>
-        <div className={s.siteMap}>
-          <span>Site map</span>
-          <span>Top-Headlines news</span>
-          <span>Weather</span>
         </div>
       </div>
       <div className={s.link}>newscollector.vercel.app</div>
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -8,6 +8,7 @@ const API_KEY = "b9b52b7cae0f713be77573ff04ebe52b";
 const Weather = () => {
   const [info, setInfo] = React.useState("");
   const [city, setCity] = React.useState("");
+  const [error, setError] = React.useState(false);
   const [fetching, setFetching] = React.useState(false);
 
   useEffect(() => {
@@ -16,10 +17,21 @@ const Weather = () => {
         await fetch(
           `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=40&appid=${API_KEY}`
         )
+          // .then((data) => data.json())
+          // .then((data) => setInfo(data))
+          // .then(setFetching(false))
+          // .then(setCity(""));
           .then((data) => data.json())
-          .then((data) => setInfo(data))
-          .then(setFetching(false))
-          .then(setCity(""));
+          .then((data) => {
+            if (data.cod != 200) {
+              setError(true);
+              setCity("");
+            } else {
+              setError(false);
+              setFetching(false);
+              setInfo(data);
+            }
+          });
       };
       getData();
     }
@@ -31,7 +43,12 @@ const Weather = () => {
       <div className="main">
         <Header city={city} setCity={setCity} setFetching={setFetching} />
         {info ? (
-          <Info info={info.city} cod={info.cod} />
+          <Info
+            info={info.city}
+            cod={info.cod}
+            error={error}
+            setError={setError}
+          />
         ) : (
           <div className="info_loader">5 day weather forecast</div>
         )}
